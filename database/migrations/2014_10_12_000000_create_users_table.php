e785cbb5e2d04e9181f113845eaa9dcc0db7a4b4
Sampath <igisampa@gmail.com>
2022-12-31 02:06:00 +0530

    User login and registration is don

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('dob')->nullable()->default(null)->comment('Date of Birth');
             $table->rememberToken();
             $table->timestamps();
         });
